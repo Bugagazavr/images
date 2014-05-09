cc9b872af18e9370dca086eac5b814e5d7c1087a
bugagazavr <kirik910@gmail.com>
2014-05-09 14:38:13 +0400

    Upgrade system ruby to patch level 481

--- builder/base/scripts/ruby.sh
+++ builder/base/scripts/ruby.sh
@@ -5,11 +5,11 @@ git clone git://github.com/sstephenson/rbenv.git /home/ubuntu/.rbenv            
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 export PATH=$PATH:/home/ubuntu/.rbenv/bin
-export RBENV_VERSION=2.0.0-p353
+export RBENV_VERSION=2.0.0-p481
 eval "$(rbenv init -)"
-CC=gcc rbenv install 2.0.0-p353
+CC=gcc rbenv install 2.0.0-p481
 rbenv rehash
-rbenv global 2.0.0-p353
+rbenv global 2.0.0-p481
 rbenv rehash
 gem install bundler
 rbenv rehash
